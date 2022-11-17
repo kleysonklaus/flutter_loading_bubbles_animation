1271c1e4ec243e2dd76182e69145c8215965e38e
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-16 22:19:46 -0500

    feat(data_backup_initial_page): basic enum to control state

--- lib/loading/data_backup_initial_page.dart
+++ lib/loading/data_backup_initial_page.dart
@@ -2,40 +2,69 @@ import 'package:flutter/material.dart';
 
 import 'loading.dart';
 
-class DataBackupInitialPage extends StatelessWidget {
+enum DataBackupState { initial, start, end }
+
+class DataBackupInitialPage extends StatefulWidget {
   const DataBackupInitialPage({Key? key}) : super(key: key);
+
+  @override
+  State<DataBackupInitialPage> createState() => _DataBackupInitialPageState();
+}
+
+class _DataBackupInitialPageState extends State<DataBackupInitialPage> {
+  DataBackupState _currentState = DataBackupState.initial;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 3,
-          child: Text(
-            "Cloud Storage",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Text(
+              "Cloud Storage",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Column(
-            children: [
-              Text("last backup:"),
-              Text("28 may 2020"),
-            ],
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                Text(
+                  "last backup:",
+                  style: TextStyle(),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "28 may 2020",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: Text("create backup"),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: mainDataBackupColor,
+          ElevatedButton(
+            onPressed: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Text("create backup"),
+            ),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: mainDataBackupColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )),
           ),
-        ),
-      ],
+          SizedBox(height: 25),
+        ],
+      ),
     );
   }
 }
